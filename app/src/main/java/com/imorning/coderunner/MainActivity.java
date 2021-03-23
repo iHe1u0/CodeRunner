b043e2149d54df28ed37b99b722ff035911d6f53
iMorning <admin@catcompany.cn>
2021-03-23 20:02:10 +0800

    fix bugs
    nothing

--- app/src/main/java/com/imorning/coderunner/MainActivity.java
+++ app/src/main/java/com/imorning/coderunner/MainActivity.java
@@ -16,10 +16,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
     }
 
     /**
