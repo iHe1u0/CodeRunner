aaa8499fa158bde8f8107a38ac33c8e9b8d341e7
iMorning <admin@catcompany.cn>
2021-03-24 20:00:57 +0800

    add dependence
    implementation project(path: ':codeEditor')

--- app/src/main/java/com/imorning/coderunner/MainActivity.java
+++ app/src/main/java/com/imorning/coderunner/MainActivity.java
@@ -5,13 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import tiiehenry.code.view.CodeEditor;
+
 public class MainActivity extends AppCompatActivity {
-
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
-
+    private CodeEditor mainEditor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,4 +20,10 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
 }
