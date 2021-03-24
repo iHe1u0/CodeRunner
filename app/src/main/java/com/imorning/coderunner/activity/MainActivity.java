17fa98c3465228a3e2a74a0daa72fc53ca196917
iMorning <admin@catcompany.cn>
2021-03-24 20:51:47 +0800

    ???

--- app/src/main/java/com/imorning/coderunner/activity/MainActivity.java
+++ app/src/main/java/com/imorning/coderunner/activity/MainActivity.java
@@ -3,18 +3,22 @@ package com.imorning.coderunner.activity;
 import android.os.Bundle;
 import android.view.View;
 
-import com.imorning.coderunner.R;
 import com.imorning.coderunner.databinding.ActivityMainBinding;
 
 public class MainActivity extends BaseActivity {
-    private ActivityMainBinding binding;
+    private ActivityMainBinding bindingView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        View view = binding.getRoot();
+        bindingView = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = bindingView.getRoot();
         setContentView(view);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
 }
